201a74b76c3a96459dadbe4b2cc05bc4dfe1a54b
Lauty128 <lautarosilverii@gmail.com>
2022-08-18 07:57:03 -0300

    add-content

--- js/script.js
+++ js/script.js
@@ -54,7 +54,6 @@ openDescriptionProject = data=>{
  /* -------------------- CERRAR MODAL DE PROYECTOS ----------------------- */
 
 containerDescriptionProject.addEventListener("click", e=>{
-    console.log(e.target)
     if(e.target.classList.contains("containerDescriptionProject")){
         e.target.firstElementChild.classList.add("hideModal")
         setTimeout(()=>{ e.target.classList.add("hideContainerModal") },250)
