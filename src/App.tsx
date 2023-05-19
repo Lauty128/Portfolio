54b7bef286b0fe03c23378944a0805b6d2b3cd44
Lauty128 <lautarosilverii@gmail.com>
2023-05-19 10:10:47 -0300

    project configured

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+//---- Dependencies
+
+//---- Styles & assets
+
+//------------- AOS Config
+  import AOS from "aos"
+  import 'aos/dist/aos.css'; 
+
+function App() {
+
+  AOS.init({ once: true })
+
+  return (
+    <>
+      <h1>PORTFOLIO</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+//---- Dependencies
+  import React from 'react'
+  import ReactDOM from 'react-dom/client'
+
+//---- Components
+  import App from './App.tsx'
+
+//---- Styles
+
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
