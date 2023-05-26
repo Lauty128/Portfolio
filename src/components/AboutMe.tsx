a114f2cb9d19bd8726a70ac680c0f35eb48f6a56
Lauty128 <lautarosilverii@gmail.com>
2023-05-26 00:26:04 -0300

    image and information upgraded

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,6 +1,6 @@
 //----- Assets
 import { FaFileAlt } from 'react-icons/fa';
-import image from '@/assets/retrato.jpg';
+import image from '@/assets/retrato.webp';
 
 //----- Components
 import { MoreInfo } from './';
@@ -16,8 +16,11 @@ export function AboutMe(){
                 <div className="AboutMe__textContainer">
                     <h3 className="AboutMe__h3">SOBRE MI</h3>
                     <p className="AboutMe__p">
-                    Hola, mi nombre es Lautaro, tengo 19 años y estoy en busca de mi primer trabajo como desarrollador web. <br />
-                    Comencé en este mundo de la programación de manera autodidacta, obteniendo información y aprovechando cualquier oportunidad para poder aprender algo nuevo.
+                    Hola, mi nombre es Lautaro, tengo 20 años y estoy en busca de mi primer trabajo como desarrollador web. 
+                    <br />
+                    Comencé en este mundo de la programación de manera autodidacta, obteniendo información y aprovechando cualquier oportunidad para poder aprender algo nuevo. 
+                    <br />
+                    <br />
                     Tengo muchas ganas de poder comenzar mi carrera profesional, obtener experiencia y aprender mucho mas
                     </p>
                     <a id="buttonProjects" href="https://drive.google.com/file/d/1npHZcspj1FUnwCMK7yjlynu9iIH5_NuZ/view?usp=sharing" 
