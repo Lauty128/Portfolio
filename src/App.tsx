294bf84b430e148549f4c9621db15c179258a75f
Lauty128 <lautarosilverii@gmail.com>
2023-06-24 10:52:47 -0300

    loading added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 //---- Dependencies
   import { BrowserRouter, Routes, Route } from 'react-router-dom';
+  import { useEffect } from 'react';
 
 //---- Components
   import { Menu, Footer, Header } from './components/static';
@@ -14,19 +15,28 @@
   import 'aos/dist/aos.css'; 
 
 function App() {
+  //---- States
+  useEffect(()=>{
+    setTimeout(()=>{
+      if(document.querySelector('.Loading')){
+        (document.querySelector('.Loading') as HTMLDivElement).remove()
+      }
+    }, 200)
+  }, [])
 
+  //---- AOS
   AOS.init({ once: true })
 
   return (
     <>
-    <BrowserRouter>
-        <Menu />
-        <Header />
-        <Routes>
-          <Route path='/' element={<MainLayout />} />
-          <Route path='/blog' element={<BlogLayout />} />
-        </Routes>
-      <Footer />
+      <BrowserRouter>
+          <Menu />
+          <Header />
+          <Routes>
+            <Route path='/' element={<MainLayout />} />
+            <Route path='/blog' element={<BlogLayout />} />
+          </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   )
