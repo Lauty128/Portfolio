a913db763be135d0473bf81d12c3ab209c3fadda
Lauty128 <lautarosilverii@gmail.com>
2023-07-04 09:55:22 -0300

    article styles upgraded and preload of related article added

--- src/components/blogPage/article/Article.tsx
+++ src/components/blogPage/article/Article.tsx
@@ -12,6 +12,8 @@ interface Props{
 
 
 export default function ArticleData({ article }:Props){
+
+
     return <section className="ArticleData">
         <Link className="ArticleData__comeBack" to={'/blog'}>
             <svg width="24px" height="24px" strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#FAEDBC"><path d="M16 12H8m0 0l3.5 3.5M8 12l3.5-3.5M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z" stroke="#FAEDBC" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></svg>
@@ -26,11 +28,27 @@ export default function ArticleData({ article }:Props){
         <Markdown className="ArticleData__content">
             { article.content }
         </Markdown>
-        <div className="ArticleData__separator"></div>
+
+
         <article className="ArticleData__mostArticlesContainer">
             <h3 className="ArticleData__h3">ARTICULOS RELACIONADOS</h3>
             <div className="ArticleData__articlesContainer">
-
+                <div className="RelatedArticle RelatedArticle--preload">
+                    <div className="RelatedArticle__image"></div>
+                    <div className="RelatedArticle__title"></div>
+                </div>
+                <div className="RelatedArticle RelatedArticle--preload">
+                    <div className="RelatedArticle__image"></div>
+                    <div className="RelatedArticle__title"></div>
+                </div>
+                <div className="RelatedArticle RelatedArticle--preload">
+                    <div className="RelatedArticle__image"></div>
+                    <div className="RelatedArticle__title"></div>
+                </div>
+                <div className="RelatedArticle RelatedArticle--preload">
+                    <div className="RelatedArticle__image"></div>
+                    <div className="RelatedArticle__title"></div>
+                </div>
             </div>
         </article>
     </section>
