fa8d48886c27d22a31dbddf4c535011e12c41674
Lauty128 <lautarosilverii@gmail.com>
2023-08-30 09:49:28 -0300

    router system in Menu component deleted

--- src/components/Static/Menu.tsx
+++ src/components/Static/Menu.tsx
@@ -1,15 +1,7 @@
-//----- Dependencies
-    import { usePagesStore } from '@/store';
-    import { Link } from 'react-router-dom'
-
 //----- Assets
     import logo from '@/assets/logo-mini.png'
-
     
-
 export function Menu(){
-    //-------------------- State
-    const { is_inicio, change_is_inicio } = usePagesStore()
     
     //-------------------- Menu Button
     const toggleButton = (e:any)=>{
@@ -28,17 +20,15 @@ export function Menu(){
 
 
     return (
-    (is_inicio)
-        ?   
         <nav className="Menu" id="Menu">
             <img src={logo} alt="logo portafolio" className="Menu__image" />
-            
+        
             <div className="Menu__button" onClick={toggleButton}>
                 <span className="Menu__buttonDiv1"></span>
                 <span className="Menu__buttonDiv2"></span>
                 <span className="Menu__buttonDiv3"></span>
             </div>
-        
+    
             <div className="Menu__links">
                 <h3 className="Menu__h3 Menu__h3--main">
                     <a onClick={toggleButton} href="#Home">INICIO</a>
@@ -52,16 +42,6 @@ export function Menu(){
                 <h3 className="Menu__h3">
                     <a onClick={toggleButton} href="#ContactMe">CONTACTAME</a>
                 </h3>
-                <h3 className="Menu__h3 Menu__h3--blog">
-                    <Link onClick={()=> change_is_inicio(false)} to={"/blog"}>BLOG</Link>
-                </h3>
             </div>
         </nav>
-        :   
-        <div className='Menu Menu--blog'>
-        <Link onClick={()=> change_is_inicio(true)} to={'/'} className='Menu--blog__link'>
-            <svg width="24px" height="24px" strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#FAEDBC"><path d="M16 12H8m0 0l3.5 3.5M8 12l3.5-3.5M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10z" stroke="#FAEDBC" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></svg>
-            <span>VOLVER A INICIO</span>
-        </Link>  
-        </div>
 )}
