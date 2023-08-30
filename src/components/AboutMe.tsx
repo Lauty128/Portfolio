9600a12c3a4c93306a22c1375b1dc60bd6b298cb
Lauty128 <lautarosilverii@gmail.com>
2023-08-30 09:49:56 -0300

    cv link changed

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -19,7 +19,7 @@ export function AboutMe(){
                     <br />
                     Puedes descargar mi curriculum y echarle un vistazo.
                     </p>
-                    <a id="buttonProjects" href='https://drive.google.com/file/d/1-Lh-_OtR0o8yPiTwcfWB5dI9AWIWVJb_/view?usp=sharing' 
+                    <a id="buttonProjects" href='https://drive.google.com/file/d/16G5KwbATs9e9Gi7dUDhFYM1pOo1hPd8j/view?usp=sharing' 
                     className="AboutMe__button">
                         <div className="AboutMe__effectButton"></div>
                         <FaFileAlt />
