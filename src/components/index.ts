0d2ab15c8dd4634e8bc488865a9b1084125b5e2e
Lauty128 <lautarosilverii@gmail.com>
2023-08-30 09:41:23 -0300

    'MoreInfo.tsx' file deleted of the barrel in the components folder

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export * from './AboutMe';
 export * from './Contact';
 export * from './Layout';
-export * from './MoreInfo';
 export * from './Projects';
 export * from './Skills';
 export * from './card';
