87ca0adcb233ad63f241e9f87633a17ea5d0f8a3
Lauty128 <lautarosilverii@gmail.com>
2023-09-23 18:30:40 -0300

    delete code about markdown component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,25 +1,10 @@
 //---- Components
 import { AboutMe, Skills, Projects, Contact } from './';
 
-import { useEffect, useState } from 'react';
-import ReactMarkdown from 'react-markdown';
-
 export function MainLayout(){
-
-    const [markdown, setMarkdown] = useState('');
-
-    useEffect(()=>{
-        const archivo = 'Mona.md';
-        (async()=>{
-            const response = await fetch(`project/${archivo}`);
-            const data = await response.text();
-            setMarkdown(data);
-        })()
-    },[])
 
     return(
         <>
-            <ReactMarkdown>{ markdown }</ReactMarkdown>
             <AboutMe/>
             <Skills/>
             <Projects/>
